3c8147e30cb0cdb8c85afa3a3d58ecd6568cb640
MinoriHaruta <hrtmnr.3072@gmail.com>
2023-02-20 15:09:18 +0900

    画面遷移

---
+++ app/src/main/java/com/haruta/harutyan/memo/EditActivity.kt
@@ -0,0 +1,18 @@
+package com.haruta.harutyan.memo
+
+import android.app.Instrumentation.ActivityResult
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.haruta.harutyan.memo.databinding.ActivityEditBinding
+import com.haruta.harutyan.memo.databinding.ActivityMainBinding
+
+class EditActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityEditBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+    }
+}
--- app/src/main/java/com/haruta/harutyan/memo/MainActivity.kt
+++ app/src/main/java/com/haruta/harutyan/memo/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.haruta.harutyan.memo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haruta.harutyan.memo.databinding.ActivityEditBinding
+import com.haruta.harutyan.memo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        val editIntent: Intent = Intent(this, EditActivity::class.java)
+
+        binding.actionButton.setOnClickListener {
+            startActivity(editIntent)
+        }
+
     }
 }
