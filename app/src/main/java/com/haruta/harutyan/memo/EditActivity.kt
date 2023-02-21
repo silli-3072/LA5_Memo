8ee8ecc4c41a761c6a2da01d284049934ef86174
MinoriHaruta <hrtmnr.3072@gmail.com>
2023-02-21 21:57:58 +0900

    全然実装上手くいかないプッシュ

--- app/src/main/java/com/haruta/harutyan/memo/EditActivity.kt
+++ app/src/main/java/com/haruta/harutyan/memo/EditActivity.kt
@@ -1,11 +1,14 @@
 package com.haruta.harutyan.memo
 
 import android.app.Instrumentation.ActivityResult
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.haruta.harutyan.memo.databinding.ActivityEditBinding
 import com.haruta.harutyan.memo.databinding.ActivityMainBinding
+import org.json.JSONArray
 
 class EditActivity : AppCompatActivity() {
 
@@ -15,7 +18,21 @@ class EditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
+        val pref: SharedPreferences = getSharedPreferences("SharePref", Context.MODE_PRIVATE)
+
         binding.saveButton.setOnClickListener {
+            val arrayList: ArrayList<String> = ArrayList()
+
+            val stringText = binding.editText.text.toString()
+
+            val editor = pref.edit()
+            //editor.putString("Memo", stringText)
+            //editor.apply()
+
+            val jsonArray = JSONArray(arrayList)
+            editor.putString("MemoArray", jsonArray.toString())
+            editor.apply()
+
             finish()
         }
 
--- app/src/main/java/com/haruta/harutyan/memo/MainActivity.kt
+++ app/src/main/java/com/haruta/harutyan/memo/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.haruta.harutyan.memo
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.haruta.harutyan.memo.databinding.ActivityEditBinding
 import com.haruta.harutyan.memo.databinding.ActivityMainBinding
 
@@ -13,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
+        //val array = listOf<String>("リスト１","リスト２","リスト３")
+        //val list = findViewById<ListView>(R.id.memo_list)
+
+        //list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, array)
+
         val editIntent: Intent = Intent(this, EditActivity::class.java)
 
         binding.actionButton.setOnClickListener {
@@ -20,4 +29,22 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val pref: SharedPreferences = getSharedPreferences("SharePref", Context.MODE_PRIVATE)
+        val str = pref.getString("MemoArray", "")
+
+        val list = findViewById<ListView>(R.id.memo_list)
+
+        list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, str)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+    }
+
 }
